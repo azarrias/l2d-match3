bc5b6a5875cb9f7e1ecb671a5f3f97b2a890b503
azarrias <adolfo.zarrias@gmail.com>
2019-07-22 18:47:28 +0200

    Add tween animation on the Y coordinate of the level label

--- src/BeginGameState.lua
+++ src/BeginGameState.lua
@@ -4,17 +4,36 @@ function BeginGameState:init()
   -- transition alpha for the foreground rectangle fade in
   self.transitionAlpha = V11 and 1 or 255
   
+  -- starting level label out of screen for tween animation
+  self.levelLabelY = -64
+  
   -- spawn a board and place it to the right
   self.board = Board(VIRTUAL_WIDTH - 272, 16)
-  self.levelLabelY = VIRTUAL_HEIGHT / 2 - 8
 end
 
 function BeginGameState:enter(param)
   self.level = param.level
   
+  -- fade in 1 second using foreground rectangle alpha
   Timer.tween(1, {
     [self] = { transitionAlpha = 0 }
-  })
+  }) 
+  
+  -- once finished, start tween animation of level label Y coord
+  :finish(function()
+    Timer.tween(0.25, {
+      [self] = { levelLabelY = VIRTUAL_HEIGHT / 2 - 8 }
+    })
+    -- pause for one second with Timer.after
+    :finish(function()
+      Timer.after(1, function()
+        -- then animate the label going down past the bottom edge
+        Timer.tween(0.25, {
+          [self] = { levelLabelY = VIRTUAL_HEIGHT + 30 }
+        })
+      end)
+    end)
+  end)
 end
 
 function BeginGameState:update(dt)
