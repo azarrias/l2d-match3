6b78d423d74e9f2667d28c76c2efb3be89924620
azarrias <adolfo.zarrias@gmail.com>
2019-07-22 18:34:10 +0200

    Use timer to fade into the begin game state

--- src/BeginGameState.lua
+++ src/BeginGameState.lua
@@ -1,19 +1,28 @@
 BeginGameState = Class{__includes = BaseState}
 
 function BeginGameState:init()
+  -- transition alpha for the foreground rectangle fade in
+  self.transitionAlpha = V11 and 1 or 255
+  
   -- spawn a board and place it to the right
   self.board = Board(VIRTUAL_WIDTH - 272, 16)
   self.levelLabelY = VIRTUAL_HEIGHT / 2 - 8
+end
+
+function BeginGameState:enter(param)
+  self.level = param.level
+  
+  Timer.tween(1, {
+    [self] = { transitionAlpha = 0 }
+  })
 end
 
 function BeginGameState:update(dt)
   if love.keyboard.keysPressed.escape then
     gStateMachine:change('start')
   end
-end
-
-function BeginGameState:enter(param)
-  self.level = param.level
+  
+  Timer.update(dt)
 end
 
 function BeginGameState:render()
@@ -25,4 +34,8 @@ function BeginGameState:render()
   love.graphics.setFont(FONTS.large)
   love.graphics.printf('Level ' .. tostring(self.level),
     0, self.levelLabelY, VIRTUAL_WIDTH, 'center')
+  
+  -- transition foreground rectangle
+  love.graphics.setColor(COLORS.white[1], COLORS.white[2], COLORS.white[3], self.transitionAlpha)
+  love.graphics.rectangle('fill', 0, 0, VIRTUAL_WIDTH, VIRTUAL_HEIGHT)
 end
